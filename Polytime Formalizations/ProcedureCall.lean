57f9ee48975a7984018afb7bdb838510f5ef1270
Quang Dao <qvd@andrew.cmu.edu>
2025-08-15 14:01:08 -0600

    some more progress

--- Polytime Formalizations/ProcedureCall.lean
+++ Polytime Formalizations/ProcedureCall.lean
@@ -10,17 +10,31 @@ import VCVio
 
 open OracleComp OracleSpec
 
-universe u
+universe u v
 
 /- Define procedure A -/
 
 /-- A has access to 2 oracles: B and C. Let's say they both take & return dummies. -/
 
+@[reducible]
 def specForCallBAndC : OracleSpec (Fin 2) := fun _ => (Unit, Unit)
+
+instance : FiniteRange specForCallBAndC := by
+  unfold specForCallBAndC
+  refine ⟨?_, ?_⟩
+  · intro i; unfold OracleSpec.range; infer_instance
+  · intro i; unfold OracleSpec.range; infer_instance
 
 /-- B has access to 1 oracle: C. Let's say it takes & returns dummies. -/
 
+@[reducible]
 def specForCallC : OracleSpec (Fin 1) := fun _ => (Unit, Unit)
+
+instance : FiniteRange specForCallC := by
+  unfold specForCallC
+  refine ⟨?_, ?_⟩
+  · intro i; unfold OracleSpec.range; infer_instance
+  · intro i; unfold OracleSpec.range; infer_instance
 
 def handleBQuery (procB : OracleComp specForCallC Unit) :
     QueryImpl specForCallBAndC (OracleComp specForCallC) where
@@ -31,45 +45,78 @@ def handleBQuery (procB : OracleComp specForCallC Unit) :
     | 1 => (query (spec := specForCallC) 0 t)
 
 /-- Orchestrator for all procedures. It will run A, and for every query A makes to B, it will run B, handling every query made to C. -/
-def procOrchestrator
+def handleProcB
     (procA : OracleComp specForCallBAndC Unit) (procB : OracleComp specForCallC Unit) :
       OracleComp specForCallC Unit :=
   simulateQ (handleBQuery procB) procA
 
-variable [MonadStep Nat (OracleComp specForCallBAndC)]
-  [LawfulMonadStep Nat (OracleComp specForCallBAndC)]
-  [MonadStep Nat (OracleComp specForCallC)]
-  [LawfulMonadStep Nat (OracleComp specForCallC)]
+def handleCQuery (procC : OracleComp []ₒ Unit) :
+    QueryImpl specForCallC (OracleComp []ₒ) where
+  impl | query i _ => match i with
+    -- If it's a call to C, we run C
+    | 0 => procC
 
+def handleProcC (proc : OracleComp specForCallC Unit) (procC : OracleComp []ₒ Unit) :
+      OracleComp []ₒ Unit :=
+  simulateQ (handleCQuery procC) proc
+
+#check simulateQ_query
+#check simulateQ_bind
+
+-- simulateQ_step
+
+-- def handleQueryIWithProc : QueryImpl specForCallProc (OracleComp []ₒ) where
+-- handle query by running the procedure, then continuing with the response
+
+-- simulateQ (handleQueryIWithProc) (step a >>= (query i t)) =
+-- step (a + cost of proc) >>= simulateQ (handleQueryIWithProc) (query i t)
+
+variable
+  -- There is a step function for computations that use no oracles
+  [MonadStep Nat (OracleComp []ₒ)]
+  -- This step function is lawful for pure and bind
+  -- `step 0 = pure ()`
+  -- `step a >>= step b = step (a + b)`
+  [LawfulMonadStep Nat (OracleComp []ₒ)]
+  -- This step function is lawful for failure: `step c >>= failure = failure >>= step c`
+  [LawfulAlternativeMonadStep Nat (OracleComp []ₒ)]
+  -- [MonadStep Nat (OracleComp specForCallBAndC)]
+  -- [LawfulMonadStep Nat (OracleComp specForCallBAndC)]
+  -- [MonadStep Nat (OracleComp specForCallC)]
+  -- [LawfulMonadStep Nat (OracleComp specForCallC)]
+
+instance {ι : Type u} {spec : OracleSpec ι} : MonadLift []ₒ.OracleQuery spec.OracleQuery where
+  monadLift | query i _ => PEmpty.elim i
+
+/-- Given a cost measure on computations that use no oracles, we can define a cost measure on computations that use any set of oracles, by lifting the cost measure. -/
+instance {ι : Type u} {spec : OracleSpec ι} : MonadStep Nat (OracleComp spec) where
+  step n := liftComp (step n : OracleComp []ₒ Unit) spec
+
+-- instance {ι : Type u} {spec : OracleSpec ι} [FiniteRange spec] :
+--     MonadStep (Nat × Nat) (OracleComp spec) where
+--   step := fun ⟨liftComp (step (n, m) : OracleComp []ₒ Unit) spec
 -- TODO: reason about the cost of the orchestrator based on the cost of the procedures
 
 -- the cost of the orchestrator is at most
 -- (cost of `procA`) + (cost of `procB`) * (number of calls from `procA` to `procB`) + (cost of `procC`) * (number of calls from `procA` to `procC` + number of calls from `procB` to `procC`)
 
---   def countQueriesToAux {n : Nat} {β} (i : Fin n) : OracleComp (fun _ => (Unit, Unit)) β → Nat
+def individualQueryBound {ι : Type u} [DecidableEq ι] {spec : OracleSpec ι} [FiniteRange spec] {α : Type v}
+    (oa : OracleComp spec α) : ι → ℕ := by
+  induction oa using OracleComp.construct' with
+  | pure x => exact 0
+  | failure => exact 0
+  | query_bind i t oa rec_n =>
+    exact (fun j => if j = i then 1 else 0) + ∑ x, rec_n x
 
+-- the cost of the orchestrator is at most
+-- (cost of `procA`) + (cost of `procB`) * (number of calls from `procA` to `procB`) + (cost of `procC`) * (number of calls from `procA` to `procC` + number of calls from `procB` to `procC`)
 
-
---     -- | .pure _ => 0
---     -- -- | .step _ k => countQueriesToAux i (k ())
---     -- | j _ k =>
---     --   let rest := countQueriesToAux i (k ())
---     --   if j = i then rest + 1 else rest
---     -- | .bind x f => countQueriesToBind i x f
-
---   def countQueriesToBind {n : Nat} {β} {γ} (i : Fin n) : OracleComp (fun _ => (Unit, Unit)) β → (β → OracleComp _ γ) → Nat
---     | .pure a, f => countQueriesToAux i (f a)
---     -- | .step _ k, f => countQueriesToBind i (k ()) f
---     | .query j _ k, f =>
---       let rest := countQueriesToBind i (k ()) f
---       if j = i then rest + 1 else rest
---     | .bind x g, f => countQueriesToBind i x (fun a => countQueriesToBind i (g a) f)
--- end
-
-def countQueriesTo {α : Type} {n : Nat} (i : Fin n) (comp : OracleComp (fun _ : Unit => (Unit, Unit)) α) : Nat :=
-  by
-    induction comp using OracleComp.construct
-
-
-lemma orchestrator_cost (procA : OracleComp specForCallBAndC Unit) (procB : OracleComp specForCallC Unit) (a b c : Nat) :
-      (IsBounded procA a) → (IsBounded procB b) →
+lemma orchestrator_cost
+    (procA : OracleComp specForCallBAndC Unit)
+    (procB : OracleComp specForCallC Unit)
+    (procC : OracleComp []ₒ Unit) (a b c : Nat)
+      (ha : IsBounded procA a) (hb : IsBounded procB b) (hc : IsBounded procC c) :
+      IsBounded (handleProcC (handleProcB procA procB) procC)
+        (a + b * individualQueryBound procA 0 +
+          c * (individualQueryBound procA 1 + individualQueryBound procB 0)) := by
+  sorry
